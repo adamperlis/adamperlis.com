1c7060ad86338d6980f582e926ed9242402e9a27
Adam Perlis <adam.perlis@gmail.com>
2015-07-07 01:27:24 -0400

    added some content to the projects

--- app/js/controllers/viewCtrl.js
+++ app/js/controllers/viewCtrl.js
@@ -19,6 +19,18 @@ $scope.$on('$stateChangeSuccess', function (event, toState) {
      $scope.invert = false; 
     }
 
+    if (toState.name === 'project3') {
+     $scope.purple = true; 
+    } else {
+     $scope.purple = false; 
+    }
+
+     if (toState.name === 'project4') {
+     $scope.green = true; 
+    } else {
+     $scope.green = false; 
+    }
+
   $scope.currentPath = $location.path();
   });
 
