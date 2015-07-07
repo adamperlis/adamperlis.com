6c84d00809a58a9258aeceea8191c52b7d44a3f9
Adam Perlis <adam.perlis@gmail.com>
2015-07-07 16:51:20 -0400

    got carousel of projects working...sort off

--- app/js/controllers/viewCtrl.js
+++ app/js/controllers/viewCtrl.js
@@ -6,35 +6,78 @@ controllersModule.controller('viewCtrl', ['$rootScope', '$scope', '$state', '$lo
 
 $rootScope.$state = $state;
 
+
+//cycle through projects #####BROKEN NEED TO FIX#######
+var route = ['project1','project2','project3','project4','project5']
+
+var index = 0
+
+    $scope.next = function () {
+        var path = route[index + 1];
+        $location.path(path);
+        // console.log("clicked right");
+        index = index + 1
+
+        if (index == 4) {
+            index = -1
+        }
+    };
+
+    $scope.prev = function () {
+        if (index == 0) {
+            index = 5
+        }
+        var path = route[index - 1];
+        $location.path(path);
+        // console.log("clicked left");
+        index = index - 1
+
+    };
+
+
 $scope.$on('$stateChangeSuccess', function (event, toState) {
   if (toState.name === 'home') {
-     $scope.back = true; 
+        $scope.back = true; 
     } else {
-     $scope.back = false; 
+        $scope.back = false; 
     }
 
     if (toState.name === 'project1') {
-     $scope.navbarInvert = true; 
+        $scope.navbarInvert = true;
+        $scope.focus = true; 
     } else {
-     $scope.navbarInvert = false; 
+        $scope.navbarInvert = false;
+        $scope.focus = false;  
     }
 
     if (toState.name === 'project2') {
-     $scope.invert = true; 
+        $scope.invert = true;
+        $scope.focus = true; 
     } else {
-     $scope.invert = false; 
+        $scope.invert = false;
+        $scope.focus = false; 
     }
 
     if (toState.name === 'project3') {
-     $scope.purple = true; 
+        $scope.purple = true;
+        $scope.focus = true; 
     } else {
-     $scope.purple = false; 
+        $scope.purple = false;
+        $scope.focus = false; 
     }
 
-     if (toState.name === 'project4') {
-     $scope.green = true; 
+    if (toState.name === 'project4') {
+        $scope.green = true;
+        $scope.focus = true; 
     } else {
-     $scope.green = false; 
+        $scope.green = false;
+        $scope.focus = false; 
+    }
+
+    if (toState.name === 'project5') {
+        $scope.focus = true; 
+    } else {
+        $scope.focus = false; 
     }
 
   $scope.currentPath = $location.path();
--- app/js/directives/parallax.js
+++ app/js/directives/parallax.js
@@ -2,40 +2,40 @@
 
 var directivesModule = require('./_index.js');
 
-directivesModule.directive('parallax', ['$timeout', 'Scroller', function($timeout, Scroller) {
+// directivesModule.directive('parallax', ['$timeout', 'Scroller', function($timeout, Scroller) {
 
-    return {
-        link: function($scope, element, attrs) {
-            var height,
-                offset;
+//     return {
+//         link: function($scope, element, attrs) {
+//             var height,
+//                 offset;
 
-            element.addClass('parallax');
+//             element.addClass('parallax');
 
-            var getPositions = function() {
-                height = element[0].offsetHeight;
-                offset = Scroller.getOffset(element[0])
-            }
+//             var getPositions = function() {
+//                 height = element[0].offsetHeight;
+//                 offset = Scroller.getOffset(element[0])
+//             }
 
-            angular.element(window).bind('resize', function() {
-                getPositions();
-            });
+//             angular.element(window).bind('resize', function() {
+//                 getPositions();
+//             });
 
-            angular.element(window).bind('scroll touchmove', function() {
-                if ( ! height) {
-                    getPositions();
-                }
+//             angular.element(window).bind('scroll touchmove', function() {
+//                 if ( ! height) {
+//                     getPositions();
+//                 }
 
-                if (window.pageYOffset < 0) {
-                    element.removeClass('fixed');
-                    element.css('top', 0);
+//                 if (window.pageYOffset < 0) {
+//                     element.removeClass('fixed');
+//                     element.css('top', 0);
                     
-                    return;
-                }
+//                     return;
+//                 }
 
-                element.css('top', -(window.pageYOffset/2) + offset.top + "px")
+//                 element.css('top', -(window.pageYOffset/2) + offset.top + "px")
             
-            })
+//             })
 
-        }
-    }
-}])
+//         }
+//     }
+// }])
