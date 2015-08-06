35451ca7872af5f590b5ed83c7d38f272b884aba
Adam Perlis <adam.perlis@gmail.com>
2015-08-06 15:26:21 -0400

    trying a new approach on the server

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/build'));
+// app.use(express.static(__dirname + '/build'));
 
-// views is directory for all template files
+// // views is directory for all template files
 // app.set('/', __dirname);
 // app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+// app.get('/', function(request, response) {
+//   response.render('index');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
   response.render('index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var server = app.listen(process.env.PORT || 5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
