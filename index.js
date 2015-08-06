82a0bea147d1fd003023fcb62cb09d1268df80bf
Adam Perlis <adam.perlis@gmail.com>
2015-08-06 16:58:00 -0400

    attempt 1 billion

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use(express.static(__dirname + '/build'));
-
-// // views is directory for all template files
-// app.set('/', __dirname);
-// app.set('view engine', 'ejs');
-
-// app.get('/', function(request, response) {
-//   response.render('index');
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/build'));
 
-app.get('/', function (req, res) {
-  res.render('index.html');
+// views is directory for all template files
+app.set('/', __dirname);
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index.html');
 });
 
-var server = app.listen(process.env.PORT || 5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
