9f488b2f898a6bba7929306770906bbd3c946bc8
Adam Perlis <adam.perlis@gmail.com>
2015-08-06 15:05:23 -0400

    adjusted animation timing, still trying to get the index to point to the right place in production

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/build'));
 
 // views is directory for all template files
-app.set('/', __dirname);
-app.set('view engine', 'ejs');
+// app.set('/', __dirname);
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('index');
