967e83a31c6c8bc815f3d6bab1e34707860cdc20
Adam Perlis <adam.perlis@gmail.com>
2015-08-06 15:29:52 -0400

    trying a new approach on the server2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  response.render('index');
+  res.render('index');
 });
 
 var server = app.listen(process.env.PORT || 5000, function () {
