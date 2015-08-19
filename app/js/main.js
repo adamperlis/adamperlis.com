6b6d04f7a8c9c3bd533d78d6f5c7f700a91e3619
Adam Perlis <adam.perlis@gmail.com>
2015-08-19 12:03:03 -0400

    adding google analytics

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ var angular = require('angular');
 // angular modules
 require('angular-ui-router');
 require('angular-animate');
-// require('../../vendor/angular-swipe-master/src/angular-swipe.js');
+require('angulartics');
 require('./templates');
 require('./controllers/_index');
 require('./services/_index');
@@ -21,8 +21,9 @@ angular.element(document).ready(function() {
     'app.controllers',
     'app.services',
     'app.directives',
-    'ngAnimate'
-    // 'swipe'
+    'ngAnimate',
+    'angulartics', 
+    'angulartics.google.analytics'
   ];
 
   // mount on window for testing
