f5e9135c53d90628becb57727f28ccd0d55a3962
Adam Perlis <adam.perlis@gmail.com>
2015-08-24 17:11:33 -0400

    got landscape mode to display turn around message

--- app/js/controllers/viewCtrl.js
+++ app/js/controllers/viewCtrl.js
@@ -4,24 +4,63 @@ var controllersModule = require('./_index');
 
 controllersModule.controller('viewCtrl', ['$rootScope', '$scope', '$state', '$location', '$window', '$document', function($rootScope, $scope, $state, $location, $window, $document) {
 
-$rootScope.$state = $state;
+    $rootScope.$state = $state;
 
 //Scroll to top on route change
 $rootScope.$on("$stateChangeSuccess", function (event, currentState, previousState) {
     $window.scrollTo(0, 0);
 });
 
-$scope.scrollTopButton = false;
+    $scope.scrollTopButton = false;
 
-angular.element($window).bind("scroll", function() {
-    if (this.pageYOffset >= 300) {
-    $scope.scrollTopButtonOn = true;
-    } else {
-    $scope.scrollTopButtonOn = false;
+    angular.element($window).bind("scroll", function() {
+        if (this.pageYOffset >= 300) {
+            $scope.scrollTopButtonOn = true;
+        } else {
+            $scope.scrollTopButtonOn = false;
+        }
+        $scope.$apply();
+    });
+
+
+        //Check screen orientation on mobile
+        $scope.orientationHorizontal = false;
+        console.log($scope.orientationHorizontal); 
+
+        $scope.$watch(function(){
+         return $window.innerWidth;
+        }, function(value) {
+         
+        $scope.windowWidth = value; 
+        console.log($scope.windowWidth);
+
+        if ( $scope.windowWidth < 1200 ) {
+
+        $scope.getWindowOrientation = function () {
+            return $window.orientation;
+        };
+
+        $scope.$watch($scope.getWindowOrientation, function (newValue, oldValue) {
+            $scope.degrees = newValue;
+
+            if ($scope.degrees === 90 || $scope.degrees === -90) {
+                $scope.orientationHorizontal = true;
+                // console.log($scope.orientationHorizontal); 
+                // console.log($scope.degrees);
+            } else {
+                $scope.orientationHorizontal = false;
+                // console.log($scope.orientationHorizontal); 
+                // console.log($scope.degrees); 
+            }
+
+
+        }, true);
+
+        angular.element($window).bind('orientationchange', function () {
+            $scope.$apply();
+        });
     }
-    $scope.$apply();
 });
-
 
 
 //makes page scroll up 
@@ -34,9 +73,9 @@ var route = ['project1','project2','project3','project4','project5']
 
 var index = 0
 
-    $scope.next = function () {
-        var path = route[index + 1];
-        $location.path(path);
+$scope.next = function () {
+    var path = route[index + 1];
+    $location.path(path);
         // console.log("clicked right");
         index = index + 1
 
@@ -69,15 +108,15 @@ var index = 0
     // };
 
 
-$scope.$on('$stateChangeSuccess', function (event, toState) {
-  if (toState.name === 'home') {
+    $scope.$on('$stateChangeSuccess', function (event, toState) {
+      if (toState.name === 'home') {
         $scope.back = true; 
     } else {
         $scope.back = false; 
     }
 
-  $scope.currentPath = $location.path();
-  });
+    $scope.currentPath = $location.path();
+});
 
 
 
